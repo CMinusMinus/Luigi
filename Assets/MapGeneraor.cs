49e882e3d4033030b0575ca1c6ab32d0c1b9f2d9
take-a-CHANCE <chancejd53@gmail.com>
2018-03-03 23:05:28 -0500

    using namespaces I guess

---
+++ Assets/MapGeneraor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ogmo {
+	public class MapGeneraor : MonoBehaviour {
+
+		public GameObject square;
+		int width;
+		int height;
+
+		string levelFolder = "Levels";
+
+		Object[] levels = Resources.LoadAll(levelFolder + "/GeneratedAssets", typeof(TextAsset));
+
+		List<OgmoLevel> levels = new List<OgmoLevel>();
+		for (int ii = 0; ii < rooms.Length; ii++){	
+			levels.Add(new OgmoLevel(rooms[ii] as OgmoLevel);
+		}	
+
+		// Use this for initialization
+		void Start () {
+			/*
+			Instantiate(square,new Vector3(1,1,0),Quaternion.identity);
+			Instantiate(square,new Vector3(2,1,0),Quaternion.identity);
+			Instantiate(square,new Vector3(1,2,0),Quaternion.identity);
+			Instantiate(square,new Vector3(2,2,0),Quaternion.identity);
+			*/
+
+
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
